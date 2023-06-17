89c762f44480ad27dd19ffaac1a0581b2321a86d
fungiking <e0308972@u.nus.edu>
2023-06-17 17:49:32 +0800

    Create try.py

---
+++ try.py
@@ -0,0 +1,15 @@
+import  numpy as np
+import matplotlib.pyplot as plt
+from quspin.operators import hamiltonian
+from quspin.basis import boson_basis_1d
+from datetime import datetime
+import pandas as pd
+from multiprocessing import Pool
+
+import math
+from pathlib import Path
+import gc
+import sys
+
+
+a=np.array([[1,2,3],[4,5,6],[1,2,3]])
