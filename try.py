a1c0d6cb8bb5ad97965c940d6c4597f30210b780
fungiking <e0308972@u.nus.edu>
2023-06-24 17:59:41 +0800

    Update try.py

--- try.py
+++ try.py
@@ -11,10 +11,17 @@ from pathlib import Path
 import gc
 import sys
 import pandas as pd
+newN = 3
+newBasisAll=boson_basis_1d(newN,Nb=2)
+newSubLatOpList=[]
+for j in range(0,newN):
+    listTmp = [[1, j]]
+    staticTmp = [["n", listTmp]]
+    opTmp = hamiltonian(staticTmp, [], dtype=np.complex128, basis=newBasisAll,check_symm=False,check_pcon=False,check_herm=False)
+    print(opTmp)
+    arrTmp = opTmp.tocsc()
+    newSubLatOpList.append(arrTmp)
 
-df = pd.DataFrame({'c1': [10, 11, 12], 'c2': [100, 110, 120]})
- # make sure indexes pair with number of rows
 
-for index, row in df.iterrows():
-    print(row[1])
+
 
